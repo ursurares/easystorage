305fa48a4d8f0b5019562a3da1e4502db586fbf6
ursurares <38881718+ursurares@users.noreply.github.com>
2020-06-06 11:28:09 +0300

    deleted useless installed_apps apps

--- easystorage/settings.py
+++ easystorage/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'store',
-    #'accounts',
-    'django_registration',
 ]
 
 MIDDLEWARE = [
